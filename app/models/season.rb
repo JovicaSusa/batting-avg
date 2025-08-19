1c8c523ade4067d29fa99ea75aa8c315920dbbe8
JovicaSusa <jovicasusa1985@gmail.com>
2025-08-20 00:13:21 +0200

    WIP - Import file

--- app/models/season.rb
+++ app/models/season.rb
@@ -1,5 +1,14 @@
 class Season < ApplicationRecord
   belongs_to :player
+  has_many :season_segments, dependent: :destroy
 
   validates :year, :hits, :at_bats, :batting_avg, presence: true
+
+  before_validation :calculate_batting_avg
+
+  private
+
+  def calculate_batting_avg
+    self.batting_avg = at_bats.zero? ? 0 : hits / at_bats
+  end
 end
