31dad624bae5a74230cc55f12deff1793a03b08a
JovicaSusa <jovicasusa1985@gmail.com>
2025-08-20 07:34:24 +0200

    Initial filtering

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,16 @@ class HomeController < ApplicationController
   include Pagy::Backend
 
   def index
-    @pagy, @seasons = pagy(Season.includes(:player, season_segments: :team))
+    result = Season.includes(:player, season_segments: :team)
+
+    if params.dig(:q, :player_id).present?
+      result = result.joins(:player).where(players: { public_id: params[:q][:player_id] })
+    end
+
+    if params.dig(:q, :year).present?
+      result = result.where(year: params[:q][:year])
+    end
+
+    @pagy, @seasons = pagy(result)
   end
 end
