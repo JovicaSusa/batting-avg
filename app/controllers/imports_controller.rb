308a2b3e9607a428a4a154b05db76e857efe93d5
JovicaSusa <jovicasusa1985@gmail.com>
2025-08-20 23:27:06 +0200

    Fix UI; Added flash

--- app/controllers/imports_controller.rb
+++ app/controllers/imports_controller.rb
@@ -2,6 +2,10 @@ class ImportsController < ApplicationController
   def create
     file_to_import = params[:csv_file]
 
-    ImportBattingStats.new(file_to_import).call if file_to_import.present?
+    return redirect_to root_path, alert: "Please upload file" unless file_to_import.present?
+
+    ImportBattingStats.new(file_to_import).call
+
+    redirect_to root_path, notice: "Yay, we did it. Here are the results!"
   end
 end
